335f3d0d3e7efad7f734408888910f324c555d6e
MarekWaligora <mwaligor69@gmail.com>
2020-03-04 18:52:45 +0100

    zadanie domowe

--- src/App.js
+++ src/App.js
@@ -1,106 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FirstComponent from './ExercFirstComponent';
-import FunctionalComponent from './FunctionalComponent';
-import './Functionalcomponent.css';
-import ClassComponent from './ClassComponent';
-import './Classcomponent.css'
-import CategoryItem from './components/categories/CategoryItem'
-
-
-class App extends Component {
- 
-
-  render() {
-    let categories=['Cacti','Tillansia','Succulent','Orchids'];
-    // METODA FOR EACH JEST ITERATOREM
-    //categories.forEach()
-    function inner(item,index,arr){
-      console.log(item);
-    }
-//Jak stworzyuc Arrow function 
-// 1. const inner=function(item,index, arr){
-//   console.log(item)
-
-// }import React from 'react';
+import React from 'react';
 import './App.css';
 import CategoryItem from './components/categories/CategoryItem';
+import axios from 'axios';
 
 class App extends React.PureComponent {
 
-  render() {
-    const categories = ['Cacti', 'Tillandsia', 'Succulents', 'Orchids', 'm'];
+  constructor(props) {
+    super(props);
+//Wartosc w konstruktorze sluzy do zainicjalizowania listy ktora bedzie napelniana danymi z bazy 
+    this.state = {
+      categories: [],
+      success:true
+    }
+    
+  }
 
+  componentDidMount() {
+    // Ustawiamy zmienna do pobrania danych w formacie json z bazy danych 
+    const requestUrl = 'http://gentle-tor-07382.herokuapp.com/categories/?format=json';
+
+
+    // pobieramy dane przy pomocy get podstawiajac adres url w stalej requestURL
+    axios.get(requestUrl)
+      // jesli sie powiedzie dla then jesli sie nie powiedzie wywolane zostanie catch
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        const categories = data.map((item) => item.name);
+        console.log(categories);
+        this.setState({ categories });
+
+      })
+      // BYl blad 
+      .catch((error) => {
+         this.setState({success:false})
+        //alert("Data couldn't be retrieved");
+      });
+  }
+
+  render() {
+    const categories = this.state.categories;
+    
     return (
       <div className="app-container">
+        
         {
-          categories.map((item, index) =>
-            <CategoryItem category={item} label='category' key={index} />
+          //jesli jest false nie powinno wyswietlac 
+          !this.state.success &&
+          <p>Nie wyświetlany element</p>
+        }
+
+        {
+          categories.map((item, index, arr) =>
+            <CategoryItem
+              category={item}
+              label='category'
+              key={index}
+              isLastItem={arr.length - 1 === index}
+              index={index}
+            />
           )
         }
+
+       
       </div>
     )
   }
 }
 
-
-export default App;
-// categories.forEach
-// // 2. Usuwamy zbedene elementy 
-// const inner=function(item){
-//   console.log(item)
-// }
-
-// 3.Zastyepujemy function porzez arrow function tj =>
-// const inner=(item)=>{
-//   console.log(item)
-// };
-// categories.forEach
-
-// // 4.Poniewaz mamy tylko 1 parametr do funkcji, usuwamy nawiasy obejmujace parametry
-// const inner=(item)=>{
-//   console.log(item)
-// };
-// categories.forEach(inner);
-
-// //5. Poniewaz w xiele funkcji mamy jedno wywolanie mozemy pozbyc sie nawiasow jklamrowych 
-// const inner=(item)=>console.log(item);
-// ;
-
-
-    //Arrow function
-   // const inner=item =>console.log(item);
- //6. Funkcja jest krotka i mozemy ja przekazac od razu do forEach    
-    //categories.map();
-    categories.forEach(item =>console.log(item));
-// for Each tylkoprzechodzi przez tablice zwraca za to map()
-
-    return (
-      <div className='app-container'>
-        {
-          //1.Mega przydatne do iterowania bez udzialu petli
-          //Metoda forEach
-          categories.map(item=><CategoryItem category={item} label="Category"/>)
-        }
-{/* 
-    Szczegolowa rozpiska jak to dziala
-        {
-          categories.map(function(item,index,arr)){
-              return (<CategoryItem={item} label="Category"/>)
-          }
-        } 
-         */}
-
-
-        <CategoryItem name='Cacti' label='Category'/>
-        <CategoryItem name='Tilantsia' label='Category'/>
-        <CategoryItem name ='Succulent'  label='Category'/>
-        <CategoryItem name='Orchids' label='Category'/>
-        
-      </div>
-    );
-  }
-}
 
 export default App;
